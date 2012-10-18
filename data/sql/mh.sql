11224e6c70a2bfd9a23d8f9819024e719b136cf4
Nicolas X9 Dachez <ndachez@epsi.fr>
2012-10-18 17:57:11 +0200

    Ajout de la BDD

--- data/sql/mh.sql
+++ data/sql/mh.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Serveur: localhost
--- Généré le : Mer 10 Octobre 2012 à 17:56
+-- Généré le : Jeu 18 Octobre 2012 à 17:56
 -- Version du serveur: 5.1.63
 -- Version de PHP: 5.3.5-1ubuntu7.11
 
@@ -209,7 +209,6 @@ CREATE TABLE IF NOT EXISTS `peanut_item` (
   `menu` bigint(20) NOT NULL,
   `author` bigint(20) NOT NULL,
   `status` varchar(255) COLLATE utf8_unicode_ci DEFAULT 'draft',
-  `url` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `relation` bigint(20) DEFAULT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
@@ -224,11 +223,11 @@ CREATE TABLE IF NOT EXISTS `peanut_item` (
 -- Contenu de la table `peanut_item`
 --
 
-INSERT INTO `peanut_item` (`id`, `type`, `menu`, `author`, `status`, `url`, `relation`, `created_at`, `updated_at`, `position`, `seo_id`) VALUES
-(1, 'page', 1, 1, 'publish', NULL, NULL, '2011-04-04 00:00:00', '2011-06-15 23:18:22', 1, 1),
-(2, 'link', 1, 1, 'draft', 'http://www.google.com', 1, '2011-04-04 00:00:00', '2011-06-15 23:18:29', 2, 2),
-(3, 'link', 2, 1, 'draft', 'http://www.peanut.fr', 2, '2011-04-04 00:00:00', '2011-06-15 23:18:37', 1, 3),
-(4, 'link', 2, 1, 'draft', 'http://www.github.com/pocky', 3, '2011-04-12 00:00:00', '2011-06-15 23:18:40', 2, 4);
+INSERT INTO `peanut_item` (`id`, `type`, `menu`, `author`, `status`, `relation`, `created_at`, `updated_at`, `position`, `seo_id`) VALUES
+(1, 'page', 1, 1, 'publish', NULL, '2011-04-04 00:00:00', '2011-06-15 23:18:22', 1, 1),
+(2, 'link', 1, 1, 'draft', 1, '2011-04-04 00:00:00', '2011-06-15 23:18:29', 2, 2),
+(3, 'link', 2, 1, 'draft', 2, '2011-04-04 00:00:00', '2011-06-15 23:18:37', 1, 3),
+(4, 'link', 2, 1, 'draft', 3, '2011-04-12 00:00:00', '2011-06-15 23:18:40', 2, 4);
 
 -- --------------------------------------------------------
 
@@ -241,6 +240,7 @@ CREATE TABLE IF NOT EXISTS `peanut_item_translation` (
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `content` longtext COLLATE utf8_unicode_ci,
   `excerpt` longtext COLLATE utf8_unicode_ci,
+  `url` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `lang` char(2) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `slug` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`,`lang`),
@@ -251,11 +251,11 @@ CREATE TABLE IF NOT EXISTS `peanut_item_translation` (
 -- Contenu de la table `peanut_item_translation`
 --
 
-INSERT INTO `peanut_item_translation` (`id`, `title`, `content`, `excerpt`, `lang`, `slug`) VALUES
-(1, 'Accueil', '<p>\r\n	Bonjour à tous,</p>\r\n<p>\r\n	Vous trouverez ici une démonstration des différents modules de peanut à travers leurs fonctionnalités. Le moteur est actuellement à considérer en version 4.1.1 et subit un refactoring complet. peanut est proposé sous la license MIT permettant à tous de modifier le code, exploiter le code et même changer de license sans contraintes.</p>\r\n<p>\r\n	peanut possède un backoffice complet, accessible avec identifiant et mot de passe permettant à tous, même néophyte, de pouvoir exploiter un site Internet. Chacun des modules est également présenté, documenté et testé afin de garantir un bon fonctionnement.</p>\r\n<p>\r\n	L&#39;ensemble du code est accessible en libre téléchargement sur le repository GIT du projet. Vous pouvez également suivre l&#39;évolution de peanut à travers le blog de l&#39;auteur. Enfin, peanut est livré en l&#39;état, sans garantie de fonctionnement et/ou de fiabilité. Cela n&#39;empêche cependant pas de faire de mon mieux pour ne pas livrer quelque chose d&#39;inutilisable.</p>\r\n<p>\r\n	Bien entendu, cette démonstration s&#39;agrémentera de fonctionnalités avec le temps alors en attendant, n&#39;hésitez pas à explorer le code !</p>\r\n', '', 'en', 'accueil'),
-(2, 'contact', '', NULL, 'en', 'contact'),
-(3, 'peanut.fr', '', NULL, 'en', 'peanut'),
-(4, 'github', '', NULL, 'en', 'github');
+INSERT INTO `peanut_item_translation` (`id`, `title`, `content`, `excerpt`, `url`, `lang`, `slug`) VALUES
+(1, 'Accueil', '<p>\r\n	Bonjour à tous,</p>\r\n<p>\r\n	Vous trouverez ici une démonstration des différents modules de peanut à travers leurs fonctionnalités. Le moteur est actuellement à considérer en version 4.1.1 et subit un refactoring complet. peanut est proposé sous la license MIT permettant à tous de modifier le code, exploiter le code et même changer de license sans contraintes.</p>\r\n<p>\r\n	peanut possède un backoffice complet, accessible avec identifiant et mot de passe permettant à tous, même néophyte, de pouvoir exploiter un site Internet. Chacun des modules est également présenté, documenté et testé afin de garantir un bon fonctionnement.</p>\r\n<p>\r\n	L&#39;ensemble du code est accessible en libre téléchargement sur le repository GIT du projet. Vous pouvez également suivre l&#39;évolution de peanut à travers le blog de l&#39;auteur. Enfin, peanut est livré en l&#39;état, sans garantie de fonctionnement et/ou de fiabilité. Cela n&#39;empêche cependant pas de faire de mon mieux pour ne pas livrer quelque chose d&#39;inutilisable.</p>\r\n<p>\r\n	Bien entendu, cette démonstration s&#39;agrémentera de fonctionnalités avec le temps alors en attendant, n&#39;hésitez pas à explorer le code !</p>\r\n', '', NULL, 'fr', 'accueil'),
+(2, 'contact', '', NULL, 'http://www.google.com', 'fr', 'contact'),
+(3, 'peanut.fr', '', NULL, 'http://www.peanut.fr', 'fr', 'peanut'),
+(4, 'github', '', NULL, 'http://www.github.com/pocky', 'fr', 'github');
 
 -- --------------------------------------------------------
 
@@ -265,20 +265,22 @@ INSERT INTO `peanut_item_translation` (`id`, `title`, `content`, `excerpt`, `lan
 
 CREATE TABLE IF NOT EXISTS `peanut_menu` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `slug` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `root_id` bigint(20) DEFAULT NULL,
   `lft` int(11) DEFAULT NULL,
   `rgt` int(11) DEFAULT NULL,
   `level` smallint(6) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `peanut_menu_sluggable_idx` (`slug`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
 
 --
 -- Contenu de la table `peanut_menu`
 --
 
-INSERT INTO `peanut_menu` (`id`, `root_id`, `lft`, `rgt`, `level`) VALUES
-(1, 1, 1, 4, 0),
-(2, 2, 1, 2, 0);
+INSERT INTO `peanut_menu` (`id`, `slug`, `root_id`, `lft`, `rgt`, `level`) VALUES
+(1, 'topmenu', 1, 1, 4, 0),
+(2, 'footermenu', 2, 1, 2, 0);
 
 -- --------------------------------------------------------
 
@@ -362,7 +364,7 @@ CREATE TABLE IF NOT EXISTS `peanut_settings` (
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `value` longtext COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=22 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=33 ;
 
 --
 -- Contenu de la table `peanut_settings`
@@ -379,17 +381,28 @@ INSERT INTO `peanut_settings` (`id`, `name`, `value`) VALUES
 (8, 'google_mail', ''),
 (9, 'google_password', ''),
 (10, 'google_tracking', ''),
-(11, 'google_guid', ''),
+(11, 'google_guid_wt', ''),
 (12, 'news_feed', 'https://github.com/pocky/peanut2/commits/master.atom'),
 (13, 'meta_title', ''),
 (14, 'meta_description', ''),
 (15, 'meta_keywords', ''),
 (16, 'meta_language', 'fr_FR'),
 (17, 'meta_robots', 'index, follow'),
-(18, 'lang', ''),
+(18, 'lang', 'a:2:{s:4:"lang";a:1:{i:0;s:2:"FR";}s:4:"trad";s:9:"Français";}'),
 (19, 'firstlang', 'FR'),
-(20, 'project_per_page', '2'),
-(21, 'interface', 'a:2:{s:5:"title";s:18:"Peanut2 Revolution";s:5:"color";s:7:"#f0017a";}');
+(20, 'contact_from', 'noreply@peanut2.local'),
+(21, 'webmaster_name', 'webmaster'),
+(22, 'webmaster_mail', 'webmaster@peanut2.local'),
+(23, 'subject', 'Nouveau message de peanut2.local'),
+(24, 'map_name', 'googlemap'),
+(25, 'map_center', 'Paris'),
+(26, 'map_address', '77 boulebard Exelmans 75016 Paris'),
+(27, 'map_zoom', '13'),
+(28, 'map_size', '512x288'),
+(29, 'google_guid_ga', ''),
+(30, 'domain_name', 'peanut.local'),
+(31, 'project_per_page', '2'),
+(32, 'interface', 'a:4:{s:5:"title";s:18:"Peanut Révolution";s:5:"color";s:7:"#000000";s:4:"logo";s:0:"";s:10:"background";s:16:"default_font.jpg";}');
 
 -- --------------------------------------------------------
 
@@ -454,14 +467,16 @@ CREATE TABLE IF NOT EXISTS `sf_guard_group` (
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
 
 --
 -- Contenu de la table `sf_guard_group`
 --
 
 INSERT INTO `sf_guard_group` (`id`, `name`, `description`, `created_at`, `updated_at`) VALUES
-(1, 'Administrateur', 'Le groupe des administrateurs', '2011-03-15 12:05:45', '2011-03-15 12:05:45');
+(1, 'Administrateur', 'Le groupe des administrateurs', '2011-03-15 12:05:45', '2011-03-15 12:05:45'),
+(2, 'Contributeurs', 'Groupe rassemblant les différents contributeurs au site Internet', '2011-08-17 15:36:57', '2012-03-07 11:47:37'),
+(3, 'Rédacteurs', 'Groupe rassemblant les différents rédacteurs au site Internet', '2011-10-25 10:23:22', '2012-03-07 11:48:16');
 
 -- --------------------------------------------------------
 
@@ -483,7 +498,11 @@ CREATE TABLE IF NOT EXISTS `sf_guard_group_permission` (
 --
 
 INSERT INTO `sf_guard_group_permission` (`group_id`, `permission_id`, `created_at`, `updated_at`) VALUES
-(1, 1, '2011-03-15 12:05:45', '2011-03-15 12:05:45');
+(1, 4, '2011-10-24 16:01:27', '2011-10-24 16:01:27'),
+(1, 5, '2011-10-24 16:01:27', '2011-10-24 16:01:27'),
+(1, 6, '2011-10-24 16:01:27', '2011-10-24 16:01:27'),
+(2, 2, '2011-10-24 16:01:35', '2011-10-24 16:01:35'),
+(3, 3, '2012-03-07 11:48:16', '2012-03-07 11:48:16');
 
 -- --------------------------------------------------------
 
@@ -499,14 +518,19 @@ CREATE TABLE IF NOT EXISTS `sf_guard_permission` (
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=7 ;
 
 --
 -- Contenu de la table `sf_guard_permission`
 --
 
 INSERT INTO `sf_guard_permission` (`id`, `name`, `description`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'La permissions permettant d''accéder au backoffice', '2011-03-15 12:04:12', '2011-03-15 12:04:12');
+(1, 'Droit de lecture', 'Autorise l''accès aux articles en tant que lecteur', '2011-10-14 11:13:05', '2012-03-07 11:41:59'),
+(2, 'Droit de contribution', 'Cette permission permet d''écrire des articles mais n''autorise pas la suppression ou l''interaction avec l''organisation générale du site', '2011-10-14 11:13:38', '2012-03-07 11:43:53'),
+(3, 'Droit de rédaction', 'Cette permission autorise le rédacteur à interagir avec l''organisation éditoriale du site', '2011-10-21 00:00:00', '2012-03-07 11:44:48'),
+(4, 'Droit de direction', 'Cette permission permet au dirigeant d''interagir avec certains paramètres de son site', '2011-10-14 11:19:40', '2012-03-07 11:45:27'),
+(5, 'Webmaster du site', 'Cette permission est à destination du webmaster (interlocuteur technique)', '2011-10-14 11:17:32', '2011-10-20 16:12:18'),
+(6, 'Accès à l''interface d''administration', 'Cette permission permet d''autoriser l''accès à l''interface d''administration', '2011-10-14 15:17:33', '2012-03-07 11:45:41');
 
 -- --------------------------------------------------------
 
@@ -561,7 +585,7 @@ CREATE TABLE IF NOT EXISTS `sf_guard_user` (
 --
 
 INSERT INTO `sf_guard_user` (`id`, `first_name`, `last_name`, `email_address`, `username`, `algorithm`, `salt`, `password`, `is_active`, `is_super_admin`, `last_login`, `created_at`, `updated_at`) VALUES
-(1, 'Alexandre', 'Balmes', 'admin@peanut.local', 'admin', 'sha1', 'baba31155ff078e4a869d7e2c88c8cca', '5cf270d479fe4ab2cc747e704e68e572850caee7', 1, 1, '2012-10-10 17:53:35', '2011-03-15 12:06:22', '2012-10-10 17:53:35');
+(1, 'Alexandre', 'Balmes', 'admin@peanut.local', 'admin', 'sha1', '3c3afa21a276fba177c831a322e70d50', '3839b69b30c4c941953544f3bcf25d1063f8d241', 1, 1, '2012-10-18 16:02:33', '2011-03-15 12:06:22', '2012-10-18 16:02:33');
 
 -- --------------------------------------------------------
 
@@ -583,7 +607,8 @@ CREATE TABLE IF NOT EXISTS `sf_guard_user_group` (
 --
 
 INSERT INTO `sf_guard_user_group` (`user_id`, `group_id`, `created_at`, `updated_at`) VALUES
-(1, 1, '2011-03-15 12:06:22', '2011-03-15 12:06:22');
+(1, 2, '2011-10-25 10:08:12', '2011-10-25 10:08:12'),
+(1, 3, '2011-10-25 10:23:36', '2011-10-25 10:23:36');
 
 -- --------------------------------------------------------
 
@@ -605,7 +630,12 @@ CREATE TABLE IF NOT EXISTS `sf_guard_user_permission` (
 --
 
 INSERT INTO `sf_guard_user_permission` (`user_id`, `permission_id`, `created_at`, `updated_at`) VALUES
-(1, 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+(1, 1, '2012-03-07 11:49:36', '2012-03-07 11:49:36'),
+(1, 2, '2012-03-07 11:49:36', '2012-03-07 11:49:36'),
+(1, 3, '2012-03-07 11:49:36', '2012-03-07 11:49:36'),
+(1, 4, '2011-10-24 16:03:00', '2011-10-24 16:03:03'),
+(1, 5, '2011-10-21 17:03:00', '2011-10-21 17:03:00'),
+(1, 6, '2011-10-21 17:05:00', '2011-10-21 17:05:00');
 
 -- --------------------------------------------------------
 
