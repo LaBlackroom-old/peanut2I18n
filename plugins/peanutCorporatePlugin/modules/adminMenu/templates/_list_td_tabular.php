7714c7b9b490dad45ce997a8884b02c406dc2e53
Nicolas X9 Dachez <ndachez@epsi.fr>
2012-11-26 14:39:14 +0100

    Suppression des plugins Coroprate & Seo qui n'ont pas leur place au moment du clone

--- plugins/peanutCorporatePlugin/modules/adminMenu/templates/_list_td_tabular.php
+++
@@ -1,8 +0,0 @@
-<td class="sf_admin_text sf_admin_list_td_name">
-  <span class="<?php echo $peanut_menu->getNode()->isLeaf() ? 'file' : 'folder' ?>">
-    <?php echo $peanut_menu->getName() ?>
-  </span>
-</td>
-<td class="sf_admin_text sf_admin_list_td_slug">
-    <?php echo $peanut_menu->getSlug() ?>
-</td>
